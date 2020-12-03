8344b83605f3a0bb188e8d459a87e4859b42b4f2
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2020-12-03 14:45:12 +0100

    cleaned up day 3

--- src/day3.kt
+++ src/day3.kt
@@ -3,20 +3,17 @@ import java.io.File
 fun main() {
     val input = File("challenges/day3").readLines()
     val length = input[0].length
-    fun traverse(x: Int, y: Int): Int {
-        var pos = 0
+    fun traverse(dir: Pair<Int, Int>): Int {
+        var x = 0
         var trees = 0
-        for(i in input.indices step y) {
-            if(input[i][pos] == '#') trees++
-            pos = (pos + x) % length
+        for (y in input.indices step dir.second) {
+            if (input[y][x] == '#') trees++
+            x = (x + dir.first) % length
         }
         return trees
     }
-    println(traverse(3, 1))
-    val options = "1,1;3,1;5,1;7,1;1,2"
-    println(options.split(";").map {
-        val (x, y) = it.split(",").map { it.toInt() }
-        traverse(x, y)
-    }.map {it.toLong()}.reduce { i, acc ->	acc * i	}
-    )
+    println(traverse(Pair(3, 1)))
+
+    val options = listOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2))
+    println(options.map { traverse(it) }.map { it.toLong() }.reduce { acc, i -> acc * i })
 }

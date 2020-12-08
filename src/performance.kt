3dfd8202f9bb66a76c39b4d4ca8dbaf191ede036
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2020-12-08 11:39:56 +0100

    solved day 8

--- src/performance.kt
+++ src/performance.kt
@@ -7,9 +7,10 @@ import day4.main as day4
 import day5.main as day5
 import day6.main as day6
 import day7.main as day7
+import day8.main as day8
 
 fun main() {
-    val days = listOf(::day1, ::day2, ::day3, ::day4, ::day5, ::day6, ::day7)
+    val days = listOf(::day1, ::day2, ::day3, ::day4, ::day5, ::day6, ::day7, ::day8)
     System.setOut(PrintStream("/dev/null"))
     for ((i, solution) in days.withIndex())
         System.err.println("day $i: " + measureNanoTime { solution() } / 1000000 + "ms")

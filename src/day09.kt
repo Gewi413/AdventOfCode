2af71fd4d8339ab4dbee4864a8f3f54916cd17b7
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2020-12-09 11:51:07 +0100

    solved day 9

--- src/day09.kt
+++ src/day09.kt
@@ -2,6 +2,27 @@
 
 package day09
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val input = File("challenges/day9").readLines().map { it.toLong() }
+    var invalid = -1L
+    for (i in 25 until input.size) {
+        val possibles = input.subList(i - 25, i).map { it }.toSet()
+        if (!possibles.any { input[i] - it in possibles }) {
+            invalid = input[i]
+            break
+        }
+    }
+    println(invalid)
+    if (invalid == -1L) return
+    for (i in input.indices)
+        for (j in i until input.size) {
+            if (input[j] > invalid) break
+            val sub = input.subList(i, j).map { it }
+            if (sub.sum() == invalid) {
+                println(sub.minOrNull()!! + sub.maxOrNull()!!)
+                return
+            }
+        }
 }

1f404d3314037a8302f8ffcada438d7824457640
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2020-12-12 13:01:06 +0100

    solved day 12

--- src/performance.kt
+++ src/performance.kt
@@ -12,11 +12,12 @@ import day08.main as day08
 import day09.main as day09
 import day10.main as day10
 import day11.main as day11
+import day12.main as day12
 
 fun main() {
     val days = listOf(
         ::day01, ::day02, ::day03, ::day04, ::day05, ::day06,
-        ::day07, ::day08, ::day09, ::day10, ::day11
+        ::day07, ::day08, ::day09, ::day10, ::day11, ::day12
     )
     System.setOut(PrintStream(OutputStream.nullOutputStream()))
     var sum = 0L

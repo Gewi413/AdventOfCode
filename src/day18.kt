4a93fc9f1bdfb4b4a23283576f236963eaaecec6
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2020-12-18 14:42:15 +0100

    cleanup day 18 part A

--- src/day18.kt
+++ src/day18.kt
@@ -17,36 +17,24 @@ fun main() {
 }
 
 private fun evaluateA(expression: String): Long {
-    val expr = expression.trim().split(" ")
-    var out = 0L
-    var op = "+"
-    var parenthesis = ""
+    if (expression.toIntOrNull() != null) return expression.toLong()
+    val parts = mutableListOf("")
     var par = 0
-    for (thing in expr) when {
-        thing.startsWith("(") -> {
-            parenthesis += " $thing"
-            par += thing.count { it == '(' }
-        }
-        thing.endsWith(")") -> {
-            parenthesis += " $thing"
-            par -= thing.count { it == ')' }
-            if (par == 0) {
-                val value = evaluateA(parenthesis.drop(2).dropLast(1))
-                when (op) {
-                    "+" -> out += value
-                    "*" -> out *= value
-                }
-                parenthesis = ""
-            }
-        }
-        par != 0 -> parenthesis += " $thing"
-        thing == "+" || thing == "*" -> op = thing
-        else -> when (op) {
-            "+" -> out += thing.toInt()
-            "*" -> out *= thing.toInt()
-        }
+    for (char in expression.trim()) {
+        if (char == ' ' && par == 0) parts += ""
+        else parts[parts.size - 1] = parts.last() + char
+        if (char == '(') par++
+        if (char == ')') par--
     }
-    return out
+
+    for ((i, exp) in parts.withIndex()) if (exp.startsWith("("))
+        parts[i] = evaluateA(exp.drop(1).dropLast(1)).toString()
+
+    for ((i, exp) in parts.withIndex()) when (exp) {
+        "+" -> parts[i + 1] = (parts[i - 1].toLong() + parts[i + 1].toLong()).toString()
+        "*" -> parts[i + 1] = (parts[i - 1].toLong() * parts[i + 1].toLong()).toString()
+    }
+    return parts.last().toLong()
 }
 
 private fun evaluateB(expression: String): Long {

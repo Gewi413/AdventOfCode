942815b847334c476182cb5d9147fd0e1f3b12c3
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-02 19:30:01 +0100

    solved day 2

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,1 +1,3 @@
 fun List<String>.toInt() = map { it.toInt() }
+fun String.toInstruction() = run { val (ins, arg) = split(' '); ins to arg.toInt() }
+infix fun <A> Pair<A, A>.to(that: A) = Triple(first, second, that)

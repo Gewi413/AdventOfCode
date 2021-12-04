e235350020d5fa9d04e2904ed2c3097d10bb9782
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-04 16:53:52 +0100

    improved performance

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,10 +2,11 @@ object Day01 : Day(1) {
     override fun main() {
         println(solve())
         println(solve(3))
+
     }
 
-    private fun solve(averaging: Int = 1) = input
-        .toInt()
+    private val nums = input.toInt()
+    private fun solve(averaging: Int = 1) = nums
         .windowed(averaging)
         .zipWithNext()
         .count { (a, b) -> a.sum() < b.sum() }
--- src/Day04.kt
+++ src/Day04.kt
@@ -1,23 +1,25 @@
 object Day04 : Day(4) {
     override fun main() {
         val nums = input[0].split(",").toInt()
-        val boards = input
-            .joinToString("\n")
-            .split("\n\n")
-            .drop(1)
-            .map { Bingo(it) }
+        val boards = input.drop(1).chunked(6).map { Bingo(it.drop(1)) }.toMutableSet()
+        Bingo.drawn -= Bingo.drawn
         var i = 0
+        var won = false
         while (true) {
-            Bingo.drawn += nums[i++]
-            if (boards.count { it.won } == 1) println(boards.first { it.won }.score * nums[i - 1])
-            if (boards.all { it.won }) break
+            Bingo.drawn += nums[i]
+            val winners = boards.filter { it.won }.toSet()
+            boards -= winners
+            if ((winners.isNotEmpty() && !won) || boards.isEmpty()) {
+                println(winners.first().score * nums[i])
+                if (won) return
+                won = true
+            }
+            i++
         }
-        Bingo.drawn -= nums[--i]
-        println((boards.first { !it.won }.score - nums[i]) * nums[i])
     }
 
-    data class Bingo(val text: String) {
-        private val nums = text.split("\n").map { it.trim().split(Regex(" +")).toInt() }
+    class Bingo(text: List<String>) {
+        private val nums = text.map { it.trim().split(Regex(" +")).toInt() }
         val won: Boolean
             get() = nums.any { row -> row.all { it in drawn } } ||
                     nums[0].indices.any { column -> nums.all { it[column] in drawn } }
@@ -25,7 +27,7 @@ object Day04 : Day(4) {
         val score: Int get() = nums.flatten().filter { it !in drawn }.fold(0, Int::plus)
 
         companion object {
-            val drawn = mutableListOf<Int>()
+            val drawn = mutableSetOf<Int>()
         }
     }
 }

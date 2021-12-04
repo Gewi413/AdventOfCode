3d86f1ee6a00892360e182ac0f350b153dbff00c
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-04 15:39:41 +0100

    added performance metrics

--- src/Day.kt
+++ src/Day.kt
@@ -1,7 +1,9 @@
 import java.io.File
+import java.io.OutputStream
+import java.io.PrintStream
+import kotlin.system.measureNanoTime
 
-abstract class Day(id: Int) {
-
+abstract class Day(val id: Int) {
     val input = File("challenges/day$id.txt").readLines()
     abstract fun main()
 
@@ -18,8 +20,24 @@ abstract class Day(id: Int) {
             }
 
         @JvmStatic
-        fun main(unused: Array<String>) {
-            days.last().main()
+        fun main(args: Array<String>) {
+            if (args.isEmpty()) days.last().main()
+            else if ("-p" in args) {
+                val stdout = System.out
+                System.setOut(PrintStream(OutputStream.nullOutputStream()))
+                val times = days.map {
+                    it to when (val time = (0..9).sumOf { _ -> measureNanoTime { it.main() } } / 10) {
+                        in 0..999 -> "${time}ns"
+                        in 1000..999999 -> "${time / 1000}µs"
+                        in 1000000..999999999 -> "${time / 1000000}ms"
+                        in 1000000000..59999999999 -> "${time / 1000000000}s"
+                        else -> "${time / 60000000000}min"
+                    }.padStart(7)
+                }
+                System.setOut(stdout)
+
+                times.forEach { println("Day ${it.first.id}:${it.second}") }
+            }
         }
     }
 }

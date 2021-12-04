d26154e95394884b56a9240b6c7d14d88fef3444
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-04 16:53:52 +0100

    improved performance

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,21 +1,20 @@
 object Day03 : Day(3) {
     override fun main() {
-        println(input[0]
-            .indices
-            .map { bit -> input.map { it[bit] == '1' }.count { it } * 2 > input.size }
-            .joinToString("") { if (it) "1" else "0" }
-            .toInt(2)
-            .let { it * (it xor ((1 shl input[0].length) - 1)) }
-        )
-        println(solve() * solve(true))
+        val nums = input.toInt(2)
+        val length = input[0].length
+        println(input[0].indices
+            .sumBy { bit -> if (nums.sumBy { it and (1 shl bit) } * 2 > nums.size shl bit) 1 shl bit else 0 }
+            .let { it * (it xor (1 shl length) - 1) })
+
+        println(solve(nums, length - 1) * solve(nums, length - 1, true))
     }
 
-    private fun solve(flip: Boolean = false): Int {
-        var copy = input
-        for (i in input[0].indices) {
-            val keep = if ((copy.count { it[i] == '1' } * 2 >= copy.size) xor flip) '1' else '0'
-            copy = copy.filter { copy.size == 1 || it[i] == keep }
-        }
-        return copy[0].toInt(2)
+    //1082324, 1353024
+    private fun solve(nums: List<Int>, index: Int = 0, flip: Boolean = false): Int {
+        val keep = (nums.sumBy { it and (1 shl index) } * 2 >= nums.size shl index) xor flip
+        return if (nums.size <= 1) nums.first() else solve(
+            nums.filter { it and (1 shl index) != 0 == keep },
+            index - 1, flip
+        )
     }
 }

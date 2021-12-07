06dc98e2eb2c3dec3626e293743a0b033a36f945
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-07 14:39:58 +0100

    improved day 7

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,14 +1,15 @@
 import kotlin.math.abs
 
 object Day07 : Day(7) {
+    private val nums = input[0].split(",").toInt()
+    private val range = nums.minOrNull()!!..nums.maxOrNull()!!
+
     override fun main() {
-        val nums = input[0].split(",").toInt()
         listOf<(Int, Int) -> Int>(
             { a, b -> abs(a - b) },
             { a, b -> val d = abs(a - b); d * (d + 1) / 2 }
-        ).map { solve(nums, it) }.forEach { println(it) }
+        ).forEach { println(solve(it)) }
     }
 
-    private fun solve(nums: List<Int>, dist: (Int, Int) -> Int) = (nums.minOrNull()!!..nums.maxOrNull()!!)
-        .map { target -> nums.sumBy { dist(target, it) } }.minOrNull()
+    private fun solve(dist: (Int, Int) -> Int): Int = range.minOf { target -> nums.sumBy { dist(target, it) } }
 }

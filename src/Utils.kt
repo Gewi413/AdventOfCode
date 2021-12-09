63555de9d43c0e3ef839d6fa629af089091c5e01
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-09 16:40:13 +0100

    typealias for lazyness

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,4 @@
 fun List<String>.toInt(base: Int = 10) = map { it.toInt(base) }
 fun String.toInstruction() = run { val (ins, arg) = split(' '); ins to arg.toInt() }
 infix fun <A> Pair<A, A>.to(that: A) = Triple(first, second, that)
+typealias Point = Pair<Int, Int>

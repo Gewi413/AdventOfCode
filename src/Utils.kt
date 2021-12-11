b7972009cd302024baf0f6bfc4242f59db1d22e2
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-11 10:56:14 +0100

    helper function to ease parsing an Int grid

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,3 +2,9 @@ fun List<String>.toInt(base: Int = 10) = map { it.toInt(base) }
 fun String.toInstruction() = run { val (ins, arg) = split(' '); ins to arg.toInt() }
 infix fun <A> Pair<A, A>.to(that: A) = Triple(first, second, that)
 typealias Point = Pair<Int, Int>
+fun parseToIntMap(input: List<String>): Map<Point, Int> {
+    val map = mutableMapOf<Point, Int>()
+    for ((y, line) in input.withIndex()) for ((x, char) in line.withIndex())
+        map[x to y] = char - '0'
+    return map
+}

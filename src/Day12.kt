0c12b9c810d0663675f786d0339eeaaf4692e209
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-12 16:15:39 +0100

    cleaned up day 12 or made it worse - depends on whose reading it

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,30 +1,19 @@
 object Day12 : Day(12) {
     override fun main() {
         val nodes = mutableMapOf<String, MutableList<String>>()
-        input.forEach { line ->
-            val (a, b) = line.split("-")
-            if (a in nodes) nodes[a]!! += b
-            else nodes[a] = mutableListOf(b)
-            if (b in nodes) nodes[b]!! += a
-            else nodes[b] = mutableListOf(a)
+        input.forEach {
+            val (a, b) = it.split("-")
+            nodes.getOrPut(a, ::mutableListOf) += b
+            nodes.getOrPut(b, ::mutableListOf) += a
         }
         println(explore(nodes, "start"))
-        println(explore(nodes, "start", 2))
+        println(explore(nodes, "start", true))
     }
 
     private fun explore(
-        map: Map<String, List<String>>,
-        from: String,
-        maxExplorations: Int = 1,
-        already: List<String> = listOf("start")
-    ): Int {
-        if (from !in map) TODO("corrupted map")
-        if (from == "end") return 1
-        val new = map[from]!!.mapNotNull { next ->
-            val visits = already.count { it == next }
-            if (next[0] in 'a'..'z' && visits >= maxExplorations) return@mapNotNull null
-            explore(map, next, if (next[0] in 'a'..'z' && visits == 1) 1 else maxExplorations, already + from)
-        }
-        return new.sum()
-    }
+        map: Map<String, List<String>>, node: String, allowDouble: Boolean = false, already: Set<String> = setOf()
+    ): Int = if (node == "end") 1 else map[node]!!.mapNotNull {
+        if (it == "start" || (it in already && it[0].isLowerCase() && !allowDouble)) null
+        else explore(map, it, allowDouble && (it[0].isUpperCase() || it !in already), already + node)
+    }.sum()
 }

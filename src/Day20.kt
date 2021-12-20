1b0fb85c829c1f1e6847819cbce2f5ff04f568c5
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2021-12-20 16:00:26 +0100

    removed debugging for day 20

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,7 +1,3 @@
-import java.awt.image.BufferedImage
-import java.io.File
-import javax.imageio.ImageIO
-
 object Day20 : Day(20) {
     override fun main() {
         val rules = input[0].mapIndexedNotNull { i, c -> if (c == '#') i else null }.toSet()
@@ -16,7 +12,6 @@ object Day20 : Day(20) {
         maxY += 300
         var state = initial.toSet()
         for (i in 1..50) {
-            ImageIO.write(save(state), "PNG", File("test/$i.png"))
             val out = mutableSetOf<Point>()
             for (x in minX..maxX) for (y in minY..maxY) {
                 val num = mutableListOf<Boolean>()
@@ -33,18 +28,4 @@ object Day20 : Day(20) {
             if (i == 2 || i == 50) println(state.size)
         }
     }
-}
-
-fun save(map: Set<Point>): BufferedImage {
-    val (minX, maxX) = map.map { it.first }.minMax()
-    val (minY, maxY) = map.map { it.second }.minMax()
-    val width: Int = maxX - minX
-    val height: Int = maxY - minY
-    val imageOut = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    val imageOutPixels = IntArray((width + 1) * (height + 1))
-    for (y in 0..height) for (x in 0..width) {
-        imageOutPixels[y * width + x] = if (minX + x to minY + y in map) 0x000000 else 0xFFFFFF
-    }
-    imageOut.setRGB(0, 0, width, height, imageOutPixels, 0, width)
-    return imageOut
 }

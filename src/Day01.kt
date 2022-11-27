9d0a7acbce739cd4f886988f4ce5aac198cc8cff
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2022-11-27 17:13:42 +0100

    preparing

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,5 @@
 object Day01 : Day(1) {
     override fun main() {
-        println(solve())
-        println(solve(3))
-
+        println(1)
     }
-
-    private val nums = input.toInt()
-    private fun solve(averaging: Int = 1) = nums
-        .windowed(averaging)
-        .zipWithNext()
-        .count { (a, b) -> a.sum() < b.sum() }
 }

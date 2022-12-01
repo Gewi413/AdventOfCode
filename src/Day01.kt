b6facfe45efb202c81d5a075cd740b2d5d71d69f
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2022-12-01 17:38:05 +0100

    solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,11 @@
 object Day01 : Day(1) {
     override fun main() {
-        println(1)
+        val elves = input
+            .joinToString("\n")
+            .split("\n\n")
+            .map { it.split("\n").toInt().sum() }
+            .sortedDescending()
+        println(elves[0])
+        println(elves.take(3).sum())
     }
 }

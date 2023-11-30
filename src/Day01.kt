4ca41bb6c041607d7b52d1201cab886ca310ce2b
Gewi <41789317+Gewi413@users.noreply.github.com>
2023-11-30 18:56:39 +0100

    prepare

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,11 +1,5 @@
 object Day01 : Day(1) {
     override fun main() {
-        val elves = input
-            .joinToString("\n")
-            .split("\n\n")
-            .map { it.split("\n").toInt().sum() }
-            .sortedDescending()
-        println(elves[0])
-        println(elves.take(3).sum())
+        println("foobar")
     }
 }

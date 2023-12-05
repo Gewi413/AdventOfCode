61ae893edc03476913623c49c61d14aeea539845
Gewi <41789317+Gewi413@users.noreply.github.com>
2023-12-05 18:25:18 +0100

    solved day 5 - this isn't accidental high usage. im cold

--- src/Day05.kt
+++ src/Day05.kt
@@ -2,18 +2,24 @@ object Day05 : Day(5) {
     override fun main() {
         val steps = input.joinToString("\n").split("\n\n")
         val seeds = steps.first().split(" ").drop(1).toLong()
-        var state = seeds
-        for (next in steps.drop(1)) {
-            val mappings = next.split("\n").drop(1).map {
+        val allMappings = steps.drop(1).map { line ->
+            line.split("\n").drop(1).map {
                 val (dest, source, len) = it.split(" ").toLong()
                 source until (source + len) to dest
-            }
-            state = state.map { seed ->
-                val new = mappings.firstOrNull { seed in it.first }
-                if (new == null) seed
-                else new.second + seed - new.first.first
-            }
+            }.sortedBy { it.first.first }
         }
-        println(state.minOrNull())
+
+        fun transform(seed: Long): Long {
+            var out = seed
+            allMappings.forEach { mappings ->
+                val new = mappings.firstOrNull { out in it.first }
+                if (new != null) out = new.second + out - new.first.first
+            }
+            return out
+        }
+
+        println(seeds.minOf { transform(it) })
+
+        println(seeds.chunked(2).parallelStream().mapToLong { (a, b) -> (a until a + b).minOf { transform(it) } }.min().asLong)
     }
 }

f328362ac32941d0f9fb4d54f45c8e3ea62176a9
Gewi <41789317+Gewi413@users.noreply.github.com>
2023-12-14 17:55:10 +0100

    all my homies hate multithreading

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,29 +1,42 @@
-import kotlin.system.measureTimeMillis
-
 object Day12 : Day(12) {
     override fun main() {
-        println(input.map { line ->
-            val (input, nums) = line.split(" ")
-            val rules = nums.split(",").toInt()
-            (0 until 5).joinToString("?") { input }.replace("\\.+", ".")
-                .trim('.') to ((0 until 5).flatMap { rules })
-        }.parallelStream().mapToLong {
-            var x: Long
-            val t = measureTimeMillis { x = solve(it.first, it.second) }
-            println("foobar: ${t}ms $it");
-            return@mapToLong x
-        }.sum())
+        println(solve(1))
+        println(solve(5))
     }
 
+    private fun solve(repeats: Int): Long {
+        return input.map { line ->
+            val (input, nums) = line.split(" ")
+            val rules = nums.split(",").toInt()
+            (0 until repeats).joinToString("?") { input }.replace("\\.+".toRegex(), ".")
+                .trim('.') to ((0 until repeats).flatMap { rules })
+        }.sumOf { solve(it.first.toList(), it.second) }
+    }
 
-    private fun solve(input: String, rules: List<Int>): Long {
+    private val cache = HashMap<Pair<List<Char>, List<Int>>, Long>()
+
+    private fun solve(input: List<Char>, rules: List<Int>): Long {
+        val state = input to rules
+        if (cache.contains(state)) return cache.getOrDefault(state, 0)
         val firstLen = rules.firstOrNull() ?: return if ('#' in input) 0 else 1
-        val newRules = rules.drop(1)
-        val next = input.windowed(firstLen).withIndex().takeWhile { (i, _) ->
-            '#' !in input.substring(0, i)
-        }.filter { (i, chars) ->
-            chars.all { c -> c != '.' } && input.getOrNull(i + firstLen) != '#'
-        }.map { it.index }
-        return next.sumOf { solve(input.drop(it + firstLen + 1), newRules) }
+        val x = when (input.firstOrNull()) {
+            null -> 0
+            '.' -> solve(input.drop(1), rules)
+            '#' -> {
+                if (input.size >= firstLen && input.take(firstLen)
+                        .all { it != '.' } && input.getOrNull(firstLen) != '#'
+                )
+                    solve(input.drop(firstLen + 1), rules.drop(1))
+                else 0
+            }
+
+            '?' -> {
+                solve(input.drop(1), rules) + solve(listOf('#') + input.drop(1), rules)
+            }
+
+            else -> throw NotImplementedError(input.joinToString())
+        }
+        cache[state] = x
+        return x
     }
 }

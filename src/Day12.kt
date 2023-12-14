330551f035e38ffe514ad7eb1d24594979408dd6
Gewi <41789317+Gewi413@users.noreply.github.com>
2023-12-14 16:47:49 +0100

    sunken cost fallacy

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,21 +1,29 @@
+import kotlin.system.measureTimeMillis
+
 object Day12 : Day(12) {
     override fun main() {
-        println(input.sumOf { line ->
+        println(input.map { line ->
             val (input, nums) = line.split(" ")
             val rules = nums.split(",").toInt()
-            solve(input, rules)
-        })
+            (0 until 5).joinToString("?") { input }.replace("\\.+", ".")
+                .trim('.') to ((0 until 5).flatMap { rules })
+        }.parallelStream().mapToLong {
+            var x: Long
+            val t = measureTimeMillis { x = solve(it.first, it.second) }
+            println("foobar: ${t}ms $it");
+            return@mapToLong x
+        }.sum())
     }
 
-    private fun solve(input: String, rules: List<Int>): Int {
-        return when {
-            '?' in input -> solve(input.replaceFirst('?', '.'), rules) + solve(input.replaceFirst('?', '#'), rules)
-            valid(input, rules) -> 1
-            else -> 0
-        }
-    }
 
-    private fun valid(line: String, rules: List<Int>): Boolean {
-        return line.trim('.').split("\\.+".toRegex()).map { it.length } == rules
+    private fun solve(input: String, rules: List<Int>): Long {
+        val firstLen = rules.firstOrNull() ?: return if ('#' in input) 0 else 1
+        val newRules = rules.drop(1)
+        val next = input.windowed(firstLen).withIndex().takeWhile { (i, _) ->
+            '#' !in input.substring(0, i)
+        }.filter { (i, chars) ->
+            chars.all { c -> c != '.' } && input.getOrNull(i + firstLen) != '#'
+        }.map { it.index }
+        return next.sumOf { solve(input.drop(it + firstLen + 1), newRules) }
     }
 }

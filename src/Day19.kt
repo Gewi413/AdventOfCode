9b167e3bdca21a7d91c6dec819341e2141e938ca
Gewi <41789317+Gewi413@users.noreply.github.com>
2023-12-20 17:57:06 +0100

    solved day 19

--- src/Day19.kt
+++ src/Day19.kt
@@ -17,8 +17,40 @@ object Day19 : Day(19) {
                             it is BasicTransition
                 }.target
             }
-            if(state == "A") thing.values.sum() else 0
+            if (state == "A") thing.values.sum() else 0
         })
+
+        var metastate = listOf("xmas".map { it to 1..4000 }.toMap() to "in")
+        var sum = 0L
+        while (metastate.any()) {
+            metastate = metastate.flatMap { state ->
+                val (thing, cond) = state
+                val next = rules[cond] ?: return@flatMap listOf(state)
+                val out = mutableListOf<Pair<Map<Char, IntRange>, String>>()
+                var remaining = thing
+                for (rule in next) {
+                    if (rule is BasicTransition) {
+                        out += listOf(remaining to rule.target)
+                        continue
+                    }
+                    if (rule !is ConditionalTransition) throw NotImplementedError()
+                    val a = remaining.toMutableMap()
+                    val b = remaining.toMutableMap()
+                    val start = remaining[rule.variable]!!.first
+                    val end = remaining[rule.variable]!!.last
+                    a[rule.variable] = if (rule.range.first <= start) start..rule.range.last else rule.range.first..end
+                    b[rule.variable] =
+                        if (rule.range.first <= start) rule.range.last + 1..end else start until rule.range.first
+                    remaining = b
+                    out += a to rule.target
+                }
+                out
+            }
+            sum += metastate.filter { it.second == "A" }
+                .sumOf { state -> state.first.values.fold(1L) { acc, i -> acc * i.count() } }
+            metastate = metastate.filter { it.second != "A" && it.second != "R" }
+        }
+        println(sum)
     }
 
     private fun rule(line: String): Pair<String, List<Transition>> {
@@ -27,7 +59,7 @@ object Day19 : Day(19) {
         return foo to baz.map {
             if (":" !in it) BasicTransition(it) else {
                 val (_, a, b) = it.split(":", "<", ">")
-                val r = if ("<" in it) 0 until a.toInt() else a.toInt() + 1..1000000
+                val r = if ("<" in it) 0 until a.toInt() else a.toInt() + 1..4000
                 ConditionalTransition(it[0], r, b)
             }
         }

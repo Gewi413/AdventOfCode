29c6b1a749339cf4b00db07cf1248abe0af8b96d
Gewi <41789317+Gewi413@users.noreply.github.com>
2024-12-01 13:26:30 +0100

    solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,20 @@
+import kotlin.math.absoluteValue
+
 object Day01 : Day(1) {
     override fun main() {
+        val (first, second) = input.map { line ->
+            val (x, y) = line
+                .split("   ")
+                .map { it.toInt() }
+            x to y
+        }.unzip()
 
+        println(first.sorted().zip(second.sorted()).sumBy {
+            (it.first - it.second).absoluteValue
+        })
+
+        println(first.sumBy {
+            it * second.count { num -> num == it }
+        })
     }
 }

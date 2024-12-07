ddd73ee98357335c0b2f2a0fdf8c37b0fba60714
Gewi <41789317+Gewi413@users.noreply.github.com>
2024-12-07 13:53:19 +0100

    solved day 7 but faster

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,37 +1,36 @@
-import java.util.stream.Collectors
-
 object Day07 : Day(7) {
     override fun main() {
         val eqs = input.map {
             val parts = it.split(": ")
             parts[0].toLong() to parts[1].split(" ").map(String::toLong)
         }
-        println(eqs.filter { it.first in eval(it.second) }
+        println(eqs.filter { eval(it.second, it.first) }
             .sumOf { it.first })
 
-        println(eqs.parallelStream()
-            .filter { it.first in eval(it.second, true) }
-            .map { it.first }
-            .collect(Collectors.summingLong { it }))
-
+        println(eqs.filter { eval(it.second, it.first, true) }
+            .sumOf { it.first })
     }
 
-    private fun eval(nums: List<Long>, enableB: Boolean = false): Set<Long> {
+    private fun eval(nums: List<Long>, goal: Long, enableConcat: Boolean = false): Boolean {
         if (nums.size == 1) {
-            return nums.toSet()
+            return nums.single() == goal
+        }
+        if (nums[0] > goal) {
+            return false
         }
         val rest = nums.drop(2)
         val resA = mutableListOf(nums[0] + nums[1])
         val resB = mutableListOf(nums[0] * nums[1])
         resA.addAll(rest)
         resB.addAll(rest)
-        val res = eval(resA, enableB) + eval(resB, enableB)
+        val res = eval(resA, goal, enableConcat) || eval(resB, goal, enableConcat)
 
-        if (!enableB) {
+        if (!enableConcat) {
             return res
         }
+
         val resC = mutableListOf((nums[0].toString() + nums[1].toString()).toLong()) // performance is my passion
         resC.addAll(rest)
-        return res + eval(resC, true)
+        return res || eval(resC, goal, true)
     }
 }

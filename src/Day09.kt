f15377771d8787c99de561c25b6dfcdcd9b342af
Gewi <41789317+Gewi413@users.noreply.github.com>
2024-12-09 16:24:56 +0100

    solved day 9, but faster

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,5 @@
 object Day09 : Day(9) {
     override fun main() {
-        val disk = mutableListOf<Data>()
         val diskB = mutableListOf<DataB>()
 
         input.single().forEachIndexed { i, x ->
@@ -13,50 +12,70 @@ object Day09 : Day(9) {
                 }
 
             diskB.add(DataB(id, count))
-
-            repeat(count) {
-                disk.add(
-                    Data(id)
-                )
-            }
         }
+        val disk = convert(diskB).toMutableList()
 
-        while (disk.any { it.id == -1 }) {
+        var next = 0
+        while (next in disk.indices) {
+            while (next in disk.indices && disk[next] != -1) {
+                next++
+            }
+            if (next !in disk.indices) {
+                break
+            }
             val move = disk.removeLast()
-            if (move.id == -1) {
+            if (move == -1) {
                 continue
             }
-            disk[disk.indexOfFirst { it.id == -1 }] = move
+
+            disk[next] = move
         }
 
-        println(disk.withIndex().sumOf { (i, x) -> i * x.id.toLong() })
-
+        println(checksum(disk))
 
         for (i in input.single().length / 2 downTo 0) {
             val move = diskB.indexOfFirst { it.id == i }
-            val free = diskB.indexOfFirst { it.id == -1 && it.size >= diskB[move].size }
+            var free = -1
+            for (j in diskB.indices) {
+                if (diskB[j].id == -1 && diskB[j].size >= diskB[move].size) {
+                    free = j
+                    break // goto anyone?
+                }
+                if (j > move) {
+                    break
+                }
+            }
             if (free == -1 || free > move) {
                 continue
             }
-
+            if (diskB[free].size == diskB[move].size) {
+                diskB[free] = DataB(diskB[move].id, diskB[free].size)
+                diskB[move] = DataB(-1, diskB[free].size)
+                continue
+            }
             diskB.add(free + 1, DataB(-1, diskB[free].size - diskB[move].size))
             diskB[free] = diskB[move + 1]
             diskB[move + 1] = DataB(-1, diskB[move + 1].size)
         }
 
-        println(
-            diskB
-                .flatMap { file -> (0 until file.size).map { Data(file.id) } }
-                .withIndex()
-                .sumOf { (i, x) ->
-                    if (x.id == -1) {
-                        0
-                    } else {
-                        i * x.id.toLong()
-                    }
-                })
+        println(checksum(convert(diskB)))
+        println(6398096697992)
     }
 
     data class Data(val id: Int)
     data class DataB(val id: Int, val size: Int)
+
+    private fun convert(disk: List<DataB>): List<Int> {
+        return disk.flatMap { file -> (0 until file.size).map { file.id } }
+    }
+
+    private fun checksum(disk: List<Int>): Long {
+        return disk.withIndex().sumOf { (i, x) ->
+            if (x == -1) {
+                0
+            } else {
+                i * x.toLong()
+            }
+        }
+    }
 }

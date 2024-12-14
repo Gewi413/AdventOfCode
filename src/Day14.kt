c327570c2d02317741be9107378b9748e1f54ff2
Gewi <41789317+Gewi413@users.noreply.github.com>
2024-12-14 22:45:07 +0100

    hinge the solve

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,6 +1,4 @@
-import java.awt.image.BufferedImage
 import java.io.File
-import javax.imageio.ImageIO
 
 object Day14 : Day(14) {
     override fun main() {
@@ -17,7 +15,13 @@ object Day14 : Day(14) {
         }
         val width = 101
         val height = 103
-        for (i in 0..20000) {
+        var cycle = 0 to 0
+        for (i in 1..20000) {
+            robots.forEach {
+                // all my homies hate java for not mathematically doing modulo
+                it.pos = (it.pos.first + it.vel.first + 100 * width) % width to
+                        (it.pos.second + it.vel.second + 100 * height) % height
+            }
             if (i == 100) {
                 println(robots.groupBy {
                     when {
@@ -32,30 +36,26 @@ object Day14 : Day(14) {
                     .map { it.value.size.toLong() }
                     .reduce { acc, j -> acc * j })
             }
-            ImageIO.write(render(robots.map { it.pos }.toSet()), "BMP", File("/tmp/aoc/$i.bmp"))
-            robots.forEach {
-                // all my homies hate java for not mathematically doing modulo
-                it.pos = (it.pos.first + it.vel.first + 100 * width) % width to
-                        (it.pos.second + it.vel.second + 100 * height) % height
+            if (robots.groupBy { it.pos.first }.filter { it.value.size > 30 }.size == 2) {
+                cycle = i to cycle.second
+                if(cycle.second != 0) {
+                    break
+                }
+            }
+            if (robots.groupBy { it.pos.second }.filter { it.value.size > 30 }.size == 2) {
+                cycle = cycle.first to i
+                if(cycle.first != 0) {
+                    break
+                }
             }
         }
-        println("the answer is somewhere in your /tmp/aoc folder ¯\\_(ツ)_/¯")
-
+        // cba, but https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+        for(i in 1..width * height) {
+            if(i % width == cycle.first && i % height == cycle.second) {
+                println(i)
+            }
+        }
     }
 
     data class Robot(var pos: Point, val vel: Point)
-
-    private fun render(map: Set<Point>): BufferedImage {
-        val (minX, maxX) = map.map { it.first }.minMax()
-        val (minY, maxY) = map.map { it.second }.minMax()
-        val width: Int = maxX - minX
-        val height: Int = maxY - minY
-        val imageOut = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-        val imageOutPixels = IntArray((width + 1) * (height + 1))
-        for (y in 0..height) for (x in 0..width) {
-            imageOutPixels[y * width + x] = if (minX + x to minY + y in map) 0x000000 else 0xFFFFFF
-        }
-        imageOut.setRGB(0, 0, width, height, imageOutPixels, 0, width)
-        return imageOut
-    }
 }
